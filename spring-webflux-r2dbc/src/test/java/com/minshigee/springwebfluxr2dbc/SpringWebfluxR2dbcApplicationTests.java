743c1dbd5881d43b3d374ec65c3624393fccf8db
minshigee <minshigee@gmail.com>
2022-02-22 22:11:23 +0900

    init webflux-r2dbc example

---
+++ spring-webflux-r2dbc/src/test/java/com/minshigee/springwebfluxr2dbc/SpringWebfluxR2dbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minshigee.springwebfluxr2dbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxR2dbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
